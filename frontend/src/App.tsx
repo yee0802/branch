dd2794027e32a4bd2c4404046b76984bfda5846e
Kye <NovusAa12@proton.me>
2024-04-13 21:31:07 +0100

    add ThemeProvider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-screen w-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
