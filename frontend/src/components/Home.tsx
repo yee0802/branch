0be76ebce968ee4622a2bb563046742ebc470122
Kye <NovusAa12@proton.me>
2024-04-29 15:21:42 +0100

    add PostList to Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,18 @@
 import Header from "./Header";
+import Container from "./ui/Container";
+import PostList from "./PostList";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Container>
+        <div className="space-y-10">
+          <div className="flex gap-y-8 py-4 sm:py-6 lg:py-8">
+            <PostList />
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
