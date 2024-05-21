dc83df1ab4ffccbf4fc366e4a21a9578e91f5650
Kye <NovusAa12@proton.me>
2024-05-21 17:10:45 +0100

    add wildcard route to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </ThemeProvider>
   );
