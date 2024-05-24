ad34463a7c17d9882db6ae89f932773dddaccf2e
Kye <NovusAa12@proton.me>
2024-05-24 16:54:23 +0100

    update PostCard component

--- frontend/src/components/ui/PostCard.tsx
+++ frontend/src/components/ui/PostCard.tsx
@@ -1,11 +1,15 @@
 import {
   Card,
-  CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Post } from "@/interfaces/Post";
+import { Link } from "react-router-dom";
+import { buttonVariants } from "./button";
+import { cn, formatDate } from "@/lib/utils";
+import { Calendar } from "lucide-react";
 
 interface PostCardProps {
   post: Post;
@@ -14,14 +18,25 @@ interface PostCardProps {
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="flex gap-4">
         <CardTitle>{post.title}</CardTitle>
-        <CardDescription>{`@${post.author.username}`}</CardDescription>
+        <CardDescription>{post.description}</CardDescription>
       </CardHeader>
-      <CardContent>
-        <img src={post.image} />
-        <p>{post.content}</p>
-      </CardContent>
+      <CardFooter className="flex justify-between">
+        <dl>
+          <dt className="sr-only">Published on</dt>
+          <dd className="flex items-center gap-1 text-sm font-medium sm:text-base">
+            <Calendar className="h-4 w-4" />
+            <time dateTime={post.createdAt}>{formatDate(post.createdAt)}</time>
+          </dd>
+        </dl>
+        <Link
+          to={`/${post.id}`}
+          className={cn(buttonVariants({ variant: "link" }), "py-0")}
+        >
+          Read More ➟
+        </Link>
+      </CardFooter>
     </Card>
   );
 };
