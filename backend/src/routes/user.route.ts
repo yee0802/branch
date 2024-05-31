fb387a93452f1610d51424104fea1298ac24d668
Kye <NovusAa12@proton.me>
2024-05-31 16:50:09 +0100

    add /api/user/login endpoint

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller";
+import { loginUser, registerUser } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
