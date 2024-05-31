e396a71758351d44a72d096a9a91a85ef5c3f30c
Kye <NovusAa12@proton.me>
2024-05-31 14:16:16 +0100

    add getUserByUsernameDb and createUserDb

--- backend/src/domains/user.domain.ts
+++ backend/src/domains/user.domain.ts
@@ -10,3 +10,31 @@ export const getUserByEmailDb = async (email: string) =>
       email: true,
     },
   });
+
+export const getUserByUsernameDb = async (username: string) =>
+  await prisma.user.findUnique({
+    where: { username },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+    },
+  });
+
+export const createUserDb = async (
+  username: string,
+  password: string,
+  email: string
+) =>
+  await prisma.user.create({
+    data: {
+      username,
+      password,
+      email,
+    },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+    },
+  });
