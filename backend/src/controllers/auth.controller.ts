3d40a61b910a1a6b184bf8e927e9fa9b5a2d0ef4
Kye <NovusAa12@proton.me>
2024-05-31 16:49:40 +0100

    add loginUser controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import throwNewError from "../error";
 import { Request, Response } from "express";
@@ -9,7 +10,11 @@ import {
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
-    const { username, password, email } = req.body;
+    const {
+      username,
+      password,
+      email,
+    }: { username: string; password: string; email: string } = req.body;
 
     if (!username || !password || !email) {
       throwNewError("Missing fields in request body", 400);
@@ -31,6 +36,40 @@ export const registerUser = async (req: Request, res: Response) => {
     return res.status(201).send({ user: createdUser });
   } catch (err) {
     console.log("Error registering user:", err.message);
+    return res.status(err.status ?? 500).send({ error: err.message });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password }: { email: string; password: string } = req.body;
+
+    if (!email || !password) {
+      throwNewError("Missing fields in request body", 400);
+    }
+
+    const foundUser = await getUserByEmailDb(email);
+
+    if (!foundUser) {
+      throwNewError("Invalid email and/or password provided", 401);
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+
+    if (!isPasswordValid) {
+      throwNewError("Invalid email and/or password provided", 401);
+    }
+
+    const secret = process.env.JWT_SECRET;
+    const expiration = process.env.JWT_EXPIRES_IN;
+
+    const token = jwt.sign({ userId: foundUser.id }, secret, {
+      expiresIn: expiration,
+    });
+
+    return res.status(200).send({ token: token });
+  } catch (err) {
+    console.log("Error logging in user:", err.message);
     return res.status(err.status ?? 500).send({ error: err.message });
   }
 };
