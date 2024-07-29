fa745407655523d7335a7c586fe16b86eec36529
Kye <NovusAa12@proton.me>
2024-07-29 19:10:46 +0100

    add registerAPI to apiClient.ts

--- frontend/src/service/apiClient.ts
+++ frontend/src/service/apiClient.ts
@@ -1,9 +1,28 @@
 import api from "@/api/axios";
 import { PostsAxiosResponse } from "@/interfaces/PostsAxiosResponse";
 import { AxiosResponse } from "axios";
+import { handleError } from "./errorHandler";
 
 export const getAllPostsAPI = async (): Promise<PostsAxiosResponse> => {
   const res: AxiosResponse<PostsAxiosResponse> = await api.get("/posts");
 
   return res.data;
+};
+
+export const registerAPI = async (
+  username: string,
+  password: string,
+  email: string,
+) => {
+  try {
+    const data = await api.post("/user/register", {
+      email,
+      username,
+      password,
+    });
+
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
 };
