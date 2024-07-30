9917ba5d1b622657d4e91c2ecfcf6d17fc2d8943
Kye <NovusAa12@proton.me>
2024-07-30 17:39:42 +0100

    update errorHandler (frontend)

--- frontend/src/service/errorHandler.ts
+++ frontend/src/service/errorHandler.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { toast } from "react-toastify";
+import { toast } from "sonner";
 
 export const handleError = (error: unknown) => {
   if (axios.isAxiosError(error)) {
     const err = error.response;
 
     if (err?.data.error) {
-      toast.warning(err?.data.error);
+      toast.error(err?.data.error);
     }
   }
 };
