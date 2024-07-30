6dbd725734aa43c932a8037361bb26b16bba0f75
Kye <NovusAa12@proton.me>
2024-07-30 18:09:45 +0100

    add AuthProvider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound";
+import { AuthProvider } from "./context/auth";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
