fa809c848063dd0b3e2e28d77a2b9829607e5978
Kye <NovusAa12@proton.me>
2024-07-30 18:25:18 +0100

    add Login page component

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoginForm from "./ui/LoginForm";
+
+export default function Login() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="py-0">
+          <LoginForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
