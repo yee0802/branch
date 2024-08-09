8f6b4fce9e01e0251aaacbe43801e5e776c7b3c6
Kye <NovusAa12@proton.me>
2024-08-09 14:01:38 +0100

    feat(apiClient): add getPostBySlugAPI function

--- frontend/src/service/apiClient.ts
+++ frontend/src/service/apiClient.ts
@@ -2,9 +2,18 @@ import api from "@/api/axios";
 import { PostsAxiosResponse } from "@/interfaces/PostsAxiosResponse";
 import { AxiosResponse } from "axios";
 import { handleError } from "./errorHandler";
+import { PostAxiosResponse } from "@/interfaces/PostAxiosResponse";
 
 export const getAllPostsAPI = async (): Promise<PostsAxiosResponse> => {
   const res: AxiosResponse<PostsAxiosResponse> = await api.get("/posts");
+
+  return res.data;
+};
+
+export const getPostBySlugAPI = async (
+  slug: string,
+): Promise<PostAxiosResponse> => {
+  const res: AxiosResponse<PostAxiosResponse> = await api.get(`/posts/${slug}`);
 
   return res.data;
 };
