ffab9050eda9503330d2c1da1098cba761a358a4
Kye <NovusAa12@proton.me>
2024-08-09 13:53:29 +0100

    feat(api): add getPostBySlug controller

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express";
-import { getAllPostsDb } from "../domains/post.domain";
+import { getAllPostsDb, getPostBySlugDb } from "../domains/post.domain";
+import throwNewError from "../error";
 
 export const getAllPosts = async (req: Request, res: Response) => {
   const posts = await getAllPostsDb();
 
   return res.status(200).send({ posts: posts });
+};
+
+export const getPostBySlug = async (req: Request, res: Response) => {
+  try {
+    const slug: string = req.params.slug;
+
+    const post = await getPostBySlugDb(slug);
+
+    if (!post) {
+      throwNewError("No post found with provided slug.", 404);
+    }
+
+    return res.status(200).send({ post: post });
+  } catch (err) {
+    console.log("Error fetching post by slug:", err.message);
+    return res.status(err.status ?? 500).send({ error: err.message });
+  }
 };
