1d40779bc12bedff8445c92ecf6ce766de11d039
Kye <NovusAa12@proton.me>
2024-08-09 13:56:04 +0100

    feat(api): add /api/posts/:slug endpoint

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -1,8 +1,9 @@
-import { getAllPosts } from "../controllers/post.controller";
+import { getAllPosts, getPostBySlug } from "../controllers/post.controller";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", getAllPosts);
+router.get("/:slug", getPostBySlug);
 
 export default router;
