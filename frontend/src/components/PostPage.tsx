540e7c10de3ec5739320358da2108fbc02e2ad29
Kye <NovusAa12@proton.me>
2024-08-09 14:07:33 +0100

    feat: add PostPage component

---
+++ frontend/src/components/PostPage.tsx
@@ -0,0 +1,47 @@
+import { Post } from "@/interfaces/Post";
+import { getPostBySlugAPI } from "@/service/apiClient";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Container from "./ui/Container";
+import Header from "./Header";
+import ArticleSkeleton from "./ui/ArticleSkeleton";
+import PostPageContent from "./ui/PostPageContent";
+import PageNotFound from "./PageNotFound";
+
+const PostPage: React.FC = () => {
+  const [post, setPost] = useState<Post | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const params = useParams();
+  const { slug } = params;
+
+  useEffect(() => {
+    if (slug) {
+      getPostBySlugAPI(slug)
+        .then((res) => setPost(res.post))
+        .catch((err) => {
+          console.error(err);
+          setError(true);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, [slug]);
+
+  if (error) {
+    return <PageNotFound />;
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="flex justify-center gap-y-8 py-4 sm:py-6 lg:py-8 ">
+          {isLoading ? <ArticleSkeleton /> : <PostPageContent post={post} />}
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default PostPage;
