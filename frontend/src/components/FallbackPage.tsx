65ac9aff5e33e6ea4f75ed1e7fbcb388a90d2a66
Kye <NovusAa12@proton.me>
2024-08-15 16:44:18 +0100

    feat: add FallbackPage component

---
+++ frontend/src/components/FallbackPage.tsx
@@ -0,0 +1,22 @@
+import Header from "./Header";
+
+type FallbackPageProps = {
+  message: string;
+  status: number;
+};
+
+const FallbackPage = ({ message, status }: FallbackPageProps) => {
+  return (
+    <div className="flex h-screen min-h-screen w-screen flex-col">
+      <Header />
+      <div className="flex h-full flex-grow items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-6 sm:flex-row">
+          <h1 className="text-4xl font-semibold">{status}</h1>
+          <p className="text-center text-xl">{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FallbackPage;
