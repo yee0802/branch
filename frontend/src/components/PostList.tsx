3ee6857920720c954ee0e39f76c6c44087192741
Kye <NovusAa12@proton.me>
2024-08-15 16:55:35 +0100

    feat(PostList): add PostListSkeleton to PostList

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -2,23 +2,29 @@ import { getAllPostsAPI } from "@/service/apiClient";
 import { useEffect, useState } from "react";
 import PostCard from "./ui/PostCard";
 import { Post } from "@/interfaces/Post";
+import PostListSkeleton from "./ui/PostListSkeleton";
 
 const PostList = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getAllPostsAPI()
       .then((res) => {
         setPosts(res.posts);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <div className="mt-[89px] space-y-4 px-2 md:px-4">
-      {posts.map((post, idx) => (
-        <PostCard key={idx} post={post} />
-      ))}
+      {isLoading ? (
+        <PostListSkeleton />
+      ) : (
+        posts.map((post, idx) => <PostCard key={idx} post={post} />)
+      )}
     </div>
   );
 };
