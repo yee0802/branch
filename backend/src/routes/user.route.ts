587c329a9f3825a5c7a3eebe16bf8c8c31744020
Kye <NovusAa12@proton.me>
2024-08-15 15:40:02 +0100

    feat(api): add /api/user/:username endpoint

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/auth.controller";
+import { verifyToken } from "../middleware/auth.middleware";
+import { getUserByUsername } from "../controllers/user.controller";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.get("/:username", verifyToken, getUserByUsername);
 
 export default router;
