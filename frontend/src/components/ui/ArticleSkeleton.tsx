32157f2cd5bb46570964686d8d0d824386a76a94
Kye <NovusAa12@proton.me>
2024-08-23 17:52:33 +0100

    refactor: update ArticleSkeleton

--- frontend/src/components/ui/ArticleSkeleton.tsx
+++ frontend/src/components/ui/ArticleSkeleton.tsx
@@ -2,14 +2,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const ArticleSkeleton = () => {
   return (
-    <div className="mt-[89px] flex flex-col justify-center space-y-9 p-4">
+    <div className="mt-[89px] flex flex-col justify-center space-y-3 p-4">
       <Skeleton className="h-16 w-[16rem] rounded-lg sm:w-[24rem] md:w-[36rem] lg:w-[48rem]" />
+      <Skeleton className="h-8 w-[16rem] sm:w-[24rem] md:w-[36rem] lg:w-[48rem]" />
 
       <div className="space-y-4">
-        <Skeleton className="h-8 w-[12rem] sm:w-[24rem] md:w-[36rem] lg:w-[48rem]" />
-        <Skeleton className="h-6 w-[10rem] sm:w-[20rem] md:w-[32rem] lg:w-[44rem]" />
+        <Skeleton className="h-8 w-[12rem] sm:w-[20rem] md:w-[32rem] lg:w-[44rem]" />
+        <Skeleton className="h-6 w-[10rem] sm:w-[18rem] md:w-[30rem] lg:w-[42rem]" />
       </div>
     </div>
   );
 };
+
 export default ArticleSkeleton;
