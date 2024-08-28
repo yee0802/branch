6d3e8dafcdc1cfcfde650612e3e5345e8ecd30e5
Kye <NovusAa12@proton.me>
2024-08-28 17:00:11 +0100

    feat(api): add /api/posts/create-post endpoint

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -1,9 +1,16 @@
-import { getAllPosts, getPostBySlug } from "../controllers/post.controller";
+import {
+  createPost,
+  getAllPosts,
+  getPostBySlug,
+} from "../controllers/post.controller";
 import { Router } from "express";
+import { verifyToken } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/", getAllPosts);
 router.get("/:slug", getPostBySlug);
+
+router.post("/create-post", verifyToken, createPost);
 
 export default router;
