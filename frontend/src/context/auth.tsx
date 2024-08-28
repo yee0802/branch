6fb565e8168997f6f31a452500f36485b2bcdbf6
Kye <NovusAa12@proton.me>
2024-08-28 18:00:08 +0100

    feat(auth): add auto-logout on token expiration

--- frontend/src/context/auth.tsx
+++ frontend/src/context/auth.tsx
@@ -3,6 +3,7 @@ import { toast } from "sonner";
 import { loginAPI, registerAPI } from "@/service/apiClient";
 import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 type AuthContextType = {
   token: string | null;
@@ -10,7 +11,6 @@ type AuthContextType = {
   registerUser: (email: string, username: string, password: string) => void;
   loginUser: (username: string, password: string) => void;
   logout: () => void;
-  isLoggedIn: boolean;
 };
 
 type UserProfile = {
@@ -31,7 +31,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   const [user, setUser] = useState<UserProfile | null>(null);
   const [token, setToken] = useState<string | null>(null);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [isReady, setIsReady] = useState<boolean>(false);
 
   useEffect(() => {
@@ -39,12 +38,21 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     const user = localStorage.getItem("user");
 
     if (token && user) {
-      setIsLoggedIn(true);
+      setToken(token);
+      setUser(JSON.parse(user));
+
       axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+
+      const { exp }: { exp: number } = jwtDecode(token);
+      const expirationTime = exp * 1000;
+
+      if (Date.now() >= expirationTime) {
+        logout();
+      }
     }
 
     setIsReady(true);
-  }, [user]);
+  }, [navigate]);
 
   const registerUser = async (
     username: string,
@@ -68,8 +76,13 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     await loginAPI(email, password)
       .then((res) => {
         if (res) {
-          localStorage.setItem("token", res?.data.token);
-          localStorage.setItem("user", res?.data.user.username);
+          const userObj: UserProfile = {
+            id: res.data.user.id,
+            username: res.data.user.username,
+          };
+
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("user", JSON.stringify(userObj));
 
           setToken(res.data.token);
           setUser(res.data.user);
@@ -88,7 +101,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     setUser(null);
-    setIsLoggedIn(false);
     setToken("");
     axios.defaults.headers.common["Authorization"] = undefined;
     navigate("/login");
@@ -96,7 +108,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   return (
     <AuthContext.Provider
-      value={{ loginUser, user, token, logout, isLoggedIn, registerUser }}
+      value={{ loginUser, user, token, logout, registerUser }}
     >
       {isReady ? children : null}
     </AuthContext.Provider>
