db0d5e98c09a08783c3843fe87b1d62738bcfb86
Kye <NovusAa12@proton.me>
2024-08-28 16:56:38 +0100

    feat(api): add createPostDb

--- backend/src/domains/post.domain.ts
+++ backend/src/domains/post.domain.ts
@@ -1,3 +1,4 @@
+import { NewPostData } from "../types/post.types";
 import prisma from "../utils/prisma";
 
 export const getAllPostsDb = async () =>
@@ -26,6 +27,28 @@ export const getPostBySlugDb = async (slug: string) =>
     where: { slug },
     select: {
       id: true,
+      title: true,
+      content: true,
+      description: true,
+      createdAt: true,
+      author: {
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+export const createPostDb = async (data: NewPostData) =>
+  await prisma.post.create({
+    data: data,
+    select: {
+      id: true,
+      slug: true,
       title: true,
       content: true,
       description: true,
