e48ba9773e352b1e5c1365d3883fa33a6b8579b9
Kye <NovusAa12@proton.me>
2024-09-03 23:31:21 +0100

    refactor: update formatDate helper

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,12 +1,20 @@
 import { type ClassValue, clsx } from "clsx";
+import { formatDistanceToNowStrict } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(input: string | number): string {
+export function formatDate(input: string): string {
   const date = new Date(input);
+  const now = new Date();
+
+  const diff = now.getTime() - date.getTime();
+
+  if (diff < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(date, { addSuffix: true });
+  }
 
   return date.toLocaleDateString("en-GB", {
     month: "long",
