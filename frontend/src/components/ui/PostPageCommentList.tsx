7b3858d6cac92500942849d52e62d641a533b1d5
Kye <NovusAa12@proton.me>
2024-09-03 23:44:36 +0100

    refactor: update PostPageCommentList to use CommentListItem

--- frontend/src/components/ui/PostPageCommentList.tsx
+++ frontend/src/components/ui/PostPageCommentList.tsx
@@ -1,7 +1,5 @@
-import defaultProfileImage from "@/assets/Default_pfp.jpg";
-import { Link } from "react-router-dom";
-import { formatDate } from "@/lib/utils";
 import Comment from "@/interfaces/Comment";
+import CommentListItem from "./CommentListItem";
 
 type PostPageCommentsListProps = {
   comments: Comment[];
@@ -19,35 +17,7 @@ const PostPageCommentsList: React.FC<PostPageCommentsListProps> = ({
   return (
     <div className="flex flex-col gap-4">
       {comments.map((comment, idx) => (
-        <div className="flex rounded-lg border p-3" key={idx}>
-          <span className="hidden sm:inline">
-            <Link to={`/users/${comment.author.username}`}>
-              <img
-                src={defaultProfileImage}
-                alt="user-profile-image"
-                className="mr-4 rounded-full"
-                width={40}
-              />
-            </Link>
-          </span>
-
-          <div className="flex flex-col">
-            <div>
-              <Link
-                to={`/users/${comment.author.username}`}
-                className="mr-2 w-fit text-lg font-bold hover:underline"
-              >
-                {`${comment.author.firstName && comment.author.lastName ? `${comment.author.firstName} ${comment.author.lastName}` : comment.author.username}`}
-              </Link>
-
-              <span className="text-muted-foreground">
-                {formatDate(comment.createdAt)}
-              </span>
-            </div>
-
-            <p>{comment.content}</p>
-          </div>
-        </div>
+        <CommentListItem comment={comment} key={idx} />
       ))}
     </div>
   );
