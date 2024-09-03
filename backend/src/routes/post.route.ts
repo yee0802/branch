936d44f95fc88d5ae191999419844d81860128c8
Kye <NovusAa12@proton.me>
2024-09-03 23:22:10 +0100

    feat(api): add /:id/create-comment endpoint

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -6,6 +6,7 @@ import {
 } from "../controllers/post.controller";
 import { Router } from "express";
 import { verifyToken } from "../middleware/auth.middleware";
+import { createComment } from "../controllers/comment.controller";
 
 const router = Router();
 
@@ -14,5 +15,7 @@ router.get("/:slug", getPostBySlug);
 
 router.post("/create-post", verifyToken, createPost);
 router.delete("/delete-post/:id", verifyToken, deletePostById);
+
+router.patch("/:id/create-comment", verifyToken, createComment);
 
 export default router;
