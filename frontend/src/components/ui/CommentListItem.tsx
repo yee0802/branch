6d6cf170b52cfa89a6f2248bc370ebb3f3057f1f
Kye <NovusAa12@proton.me>
2024-09-04 15:47:01 +0100

    feat: add CommentOptionsButton to CommentListItem

--- frontend/src/components/ui/CommentListItem.tsx
+++ frontend/src/components/ui/CommentListItem.tsx
@@ -2,14 +2,18 @@ import Comment from "@/interfaces/Comment";
 import defaultProfileImage from "@/assets/Default_pfp.jpg";
 import { Link } from "react-router-dom";
 import { formatDate } from "@/lib/utils";
+import CommentOptionsButton from "./CommentOptionsButton";
+import useAuth from "@/hooks/useAuth";
 
 type CommentProps = {
   comment: Comment;
 };
 
 const CommentListItem: React.FC<CommentProps> = ({ comment }) => {
+  const { user } = useAuth();
+
   return (
-    <div className="flex rounded-lg border p-3">
+    <div className="group/comment relative flex rounded-lg border p-3">
       <span className="hidden sm:inline">
         <Link to={`/users/${comment.author.username}`}>
           <img
@@ -21,8 +25,8 @@ const CommentListItem: React.FC<CommentProps> = ({ comment }) => {
         </Link>
       </span>
 
-      <div className="flex flex-col">
-        <div>
+      <div className="flex flex-col gap-3">
+        <div className="flex items-center">
           <Link
             to={`/users/${comment.author.username}`}
             className="mr-2 w-fit text-lg font-bold hover:underline"
@@ -30,13 +34,21 @@ const CommentListItem: React.FC<CommentProps> = ({ comment }) => {
             {`${comment.author.firstName && comment.author.lastName ? `${comment.author.firstName} ${comment.author.lastName}` : comment.author.username}`}
           </Link>
 
-          <span className="text-muted-foreground">
+          <span className="hidden text-muted-foreground sm:inline">
             {formatDate(comment.createdAt)}
           </span>
         </div>
 
         <p>{comment.content}</p>
       </div>
+      {user!.id === comment.author.id && (
+        <div className="absolute right-3">
+          <CommentOptionsButton
+            comment={comment}
+            className="ms-auto opacity-[0.0001] group-hover/comment:opacity-100"
+          />
+        </div>
+      )}
     </div>
   );
 };
