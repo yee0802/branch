10f39d3e001ac70496feb0875088db55fb3486b6
Kye <NovusAa12@proton.me>
2024-09-14 23:04:55 +0100

    feat(api): add /api/user/:id/profile/upload-avatar endpoint

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -4,6 +4,7 @@ import { verifyToken } from "../middleware/auth.middleware";
 import {
   getUserByUsername,
   updateUserById,
+  uploadUserAvatarById,
 } from "../controllers/user.controller";
 
 const router = Router();
@@ -13,5 +14,6 @@ router.post("/login", loginUser);
 
 router.get("/:username", verifyToken, getUserByUsername);
 router.patch("/:id/profile", verifyToken, updateUserById);
+router.post("/:id/profile/upload-avatar", verifyToken, uploadUserAvatarById);
 
 export default router;
