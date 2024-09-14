bb71c9b8404d89f120670aaf4c0895d1cf35ff0e
Kye <NovusAa12@proton.me>
2024-09-14 23:10:11 +0100

    chore(types): update User & Author interfaces

--- frontend/src/interfaces/Author.ts
+++ frontend/src/interfaces/Author.ts
@@ -3,5 +3,6 @@ export interface Author {
   email: string;
   firstName: string;
   lastName: string;
+  avatarURL: string;
   username: string;
 }
--- frontend/src/interfaces/User.ts
+++ frontend/src/interfaces/User.ts
@@ -5,6 +5,8 @@ interface User {
   firstName: string;
   lastName: string;
   username: string;
+  avatarURL: string;
+  bio: string;
   email: string;
   createdAt: string;
   posts: Post[];
