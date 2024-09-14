f7178c12fd00dbe69bc9b0d716fae6bd7666f07a
Kye <NovusAa12@proton.me>
2024-09-14 23:04:15 +0100

    feat(api): add uploadUserAvatarById controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import throwNewError from "../error";
 import { getUserByUsernameDb, updateUserByIdDb } from "../domains/user.domain";
 import { UpdatedProfileData } from "../types/user.types";
+import cloudinary from "../utils/cloudinary";
 
 export const getUserByUsername = async (req: Request, res: Response) => {
   try {
@@ -30,6 +31,29 @@ export const updateUserById = async (req: Request, res: Response) => {
     return res.status(200).send({ user: updatedUser });
   } catch (err) {
     console.log("Error updating user by ID:", err.message);
+    return res.status(err.status ?? 500).send({ error: err.message });
+  }
+};
+
+export const uploadUserAvatarById = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const { imagePath } = req.body;
+
+    const uploadedImage = await cloudinary.uploader.upload(imagePath, {
+      public_id: `avatar_${id}`,
+      folder: "avatars",
+      invalidate: true,
+      overwrite: true,
+    });
+
+    await updateUserByIdDb(id, {
+      avatarURL: uploadedImage.secure_url,
+    });
+
+    return res.status(200).send({ success: true });
+  } catch (err) {
+    console.log("Error updating avatar:", err.message);
     return res.status(err.status ?? 500).send({ error: err.message });
   }
 };
