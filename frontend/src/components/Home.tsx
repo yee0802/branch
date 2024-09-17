138ff60f927e9db240bce92aa403cfa69fec8812
Kye <NovusAa12@proton.me>
2024-09-17 15:43:59 +0100

    chore: update PostList component in Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Container from "./ui/Container";
 import PostList from "./PostList";
+import { getAllPostsAPI } from "@/service/apiClient";
 
 export default function Home() {
   return (
@@ -9,7 +10,11 @@ export default function Home() {
       <Container>
         <div className="space-y-10">
           <div className="flex justify-center gap-y-8 py-4 sm:py-6 lg:py-8">
-            <PostList />
+            <PostList
+              getPosts={getAllPostsAPI}
+              isMainFeed
+              className="mt-[89px] w-full max-w-2xl space-y-4 px-2 md:px-4"
+            />
           </div>
         </div>
       </Container>
