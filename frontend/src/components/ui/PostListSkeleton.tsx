d5d2d5270d6204405ae462320e79f4e8339eae49
Kye <NovusAa12@proton.me>
2024-09-17 15:27:54 +0100

    style: update PostListSkeleton

--- frontend/src/components/ui/PostListSkeleton.tsx
+++ frontend/src/components/ui/PostListSkeleton.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const PostListSkeleton = () => {
   return (
-    <div className="min-w-screen flex w-full flex-col justify-center space-y-8">
-      <Skeleton className="h-40 w-[19rem] rounded-lg sm:w-[36rem] md:w-[40rem] lg:w-[48rem]" />
-      <Skeleton className="h-40 w-[19rem] rounded-lg sm:w-[36rem] md:w-[40rem] lg:w-[48rem]" />
-      <Skeleton className="h-40 w-[19rem] rounded-lg sm:w-[36rem] md:w-[40rem] lg:w-[48rem]" />
+    <div className="flex w-full max-w-2xl flex-col justify-center space-y-8">
+      <Skeleton className="h-40" />
+      <Skeleton className="h-40" />
+      <Skeleton className="h-40" />
     </div>
   );
 };
